8e3f50822e0bc28dc0c9728df0090da2a20c6fe4
tamilvalavan0401 <tamilvalavan@vilvanetworks.com>
2025-12-12 23:22:19 +0530

    updated packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
 import { withCloudflare } from "@cloudflare/next-on-pages/next";
 
 export default withCloudflare({
-  images: {
-    unoptimized: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  images: { unoptimized: true },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
 });
